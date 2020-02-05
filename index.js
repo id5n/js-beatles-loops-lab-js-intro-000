cf1b8cb4ff8911a1ef16800e3b1902ca3e0fa155
Ian Davall <iduality5n@gmail.com>
2020-02-05 02:35:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+let beatles = [
+  "John Lennon",
+  "Paul McCartney",
+  "Ringo Starr",
+  "George Harrison"
+];
+
+let beatlesInstruments = [
+  "Guitar",
+  "Piano",
+  "Drums",
+  "Bass"
+];
+
+var theBeatlesPlay = function(arr1, arr2) {
+  let finalArr = [];
+  for (i = 0; i < beatles.length; i++) {
+    console.log(`${beatles[i]} plays ${beatlesInstruments[i]}.`)
+  }
+};
